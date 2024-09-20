58426f5bcc4addf2bb851f185afa48b83ef58c22
Carlos Eduardo <carlos_vk2012@hotmail.com>
2024-09-20 20:07:11 -0300

    Commit ultiam aula

---
+++ Site restaurante/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', () => {
+    // Botão Learn More
+    const learnMoreBtn = document.getElementById('learnMoreBtn');
+    learnMoreBtn.addEventListener('click', () => {
+        alert('Learn more about our features!');
+    });
+
+    // Formulário de Contato
+    const contactForm = document.getElementById('contactForm');
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        alert('Thank you for contacting us!');
+        contactForm.reset();
+    });
+});
--- TelegramBot/obj/Debug/net6.0/TelegramBot.AssemblyInfo.cs
+++ TelegramBot/obj/Debug/net6.0/TelegramBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af51ad2bebe608259d7a9a2142b281b59a58076a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelegramBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
